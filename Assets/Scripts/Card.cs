07006c13d5fa77668c5ac4a011e0ff1d5382ea33
María Belén Trigueros <bel.trigueros@gmail.com>
2024-10-14 14:08:49 -0300

    05 - WIP > Added comments and renamed some things for clarity.

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-[CreateAssetMenu(fileName ="New Card", menuName ="Card")]
-public class Card : ScriptableObject
+// En esta clase vamos a implementar todo lo que refiere al comportamiento de la Carta.
+public class Card : MonoBehaviour
 {
-    public Color color;
-    public string type;
-    public int value;
+    [SerializeField] CardData card;
+
+    [SerializeField] TextMeshProUGUI typeText, valueText;
+
+    [SerializeField] Image cardImage;
+
+    void Start()
+    {
+        typeText.text = card.type;
+        valueText.text = "+" + card.value.ToString();
+        cardImage.color = card.color;
+    }
 }
