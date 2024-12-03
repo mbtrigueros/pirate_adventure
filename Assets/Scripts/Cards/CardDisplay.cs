90fe0d43003e91da954642d9f32bbb79c810801f
María Belén Trigueros <bel.trigueros@gmail.com>
2024-12-03 12:48:12 -0300

    17 - Feature: added soundfx and music assets. implemented card assets, changed values accordingly, fixed placeholder code. wip > new captain cards.

--- Assets/Scripts/Cards/CardDisplay.cs
+++ Assets/Scripts/Cards/CardDisplay.cs
@@ -10,9 +10,6 @@ public class CardDisplay : MonoBehaviour
     [SerializeField] public Image cardImage;
     [SerializeField] public TextMeshProUGUI valueText;
     [SerializeField] public TextMeshProUGUI secondValueText;
-    [SerializeField] public TextMeshProUGUI typeText;
-    [SerializeField] public TextMeshProUGUI actionText;
-    [SerializeField] public TextMeshProUGUI secondActionText;
     
     public void Start() {
     }
@@ -23,32 +20,17 @@ public class CardDisplay : MonoBehaviour
     public void SetCardAppearance(Card card) {
         if (card) {
         valueText.text = card.firstValue > 0 ? card.firstValue.ToString() : "";
-        typeText.text = card.type.ToString();
-        actionText.text = card.action.ToString();
 
-        if (secondValueText && secondActionText && card is CrewCard crewCard) {
-            secondValueText.text = crewCard.secondValue.ToString();
-            secondActionText.text = crewCard.alternativeAction.ToString();
+        if (secondValueText) {
+            if( card is CrewCard crewCard) {
+                secondValueText.text = crewCard.secondValue.ToString();
+            }
+            if (card is CaptainCard captainCard) {
+                secondValueText.text = captainCard.secondValue.ToString();
+            }
+
         }
         cardImage.sprite = card.image;
-
-                switch(card.action) {
-                case CardAction.HEALTH: 
-                    cardImage.color = Color.green;
-                    break;
-                case CardAction.MOVEMENT:
-                    cardImage.color = Color.yellow;
-                    break;
-                case CardAction.ATTACK:
-                    cardImage.color = Color.red;
-                    break;
-                case CardAction.EMPTY: 
-                    cardImage.color = Color.blue;
-                    break;
-                case CardAction.BUOY:
-                    cardImage.color = Color.magenta;
-                    break;
-            }
         }
 
     }
